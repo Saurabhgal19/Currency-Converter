6b68d9da4963836d2df39e6cb6c6d4c0cf8f5fc8
Saurabh Galpalliwar <121440363+Saurabhgal19@users.noreply.github.com>
2024-01-25 13:44:18 +0530

    complete code

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const BASE_URL =
 
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const button = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
 // for (code in countryList) {
 //     console.log(code , countryList[code])
 // }
+
+window.addEventListener("load", async () => {
+        updateExchange();
+})
+
 
  for(let select of dropdowns) {
     for(currentCode in countryList) {
@@ -29,6 +38,31 @@ const dropdowns = document.querySelectorAll(".dropdown select");
     })
  }
 
+ let updateExchange = async () => {
+    let amount = document.querySelector("form input");
+    let amountValue = amount.value;
+    console.log(amountValue);
+
+    if(amountValue === "" || amountValue < 1){
+        amountValue=1;
+        amount.value= 999;
+    }
+
+    //console.log(fromCurr.value , toCurr.value)
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    //console.log(data);
+
+    let rate = data[toCurr.value.toLowerCase()].toFixed(2);
+    
+    console.log("Rate",rate);
+
+    let finalAmount = amount.value * rate;
+
+    msg.innerText = `${amountValue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+ }
+
 
  const updateFlag = (ele) => {
     let currecyCode = ele.value;
@@ -36,4 +70,13 @@ const dropdowns = document.querySelectorAll(".dropdown select");
     let countryCode = countryList[currecyCode];
     //console.log(countryCode);
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let image = ele.parentElement.querySelector('img');
+    image.src = newSrc ;
  }
+
+ button.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateExchange();
+    
+ })
+
