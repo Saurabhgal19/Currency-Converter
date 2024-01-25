b204392f5f119c0c38788c50bdf5bddd7deeed32
Saurabh Galpalliwar <121440363+Saurabhgal19@users.noreply.github.com>
2024-01-25 12:51:32 +0530

    updating functionality pending

---
+++ script.js
@@ -0,0 +1,39 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+// for (code in countryList) {
+//     console.log(code , countryList[code])
+// }
+
+ for(let select of dropdowns) {
+    for(currentCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currentCode;
+        newOption.value=currentCode;
+
+        if(select.name === "from" && currentCode ==="USD")
+        {
+            newOption.selected ="selected";
+        }
+        else if (select.name === "to" && currentCode === "INR") {
+            newOption.selected ="selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (e)=>{
+        updateFlag(e.target);
+    })
+ }
+
+
+ const updateFlag = (ele) => {
+    let currecyCode = ele.value;
+    //console.log(currecyCode);
+    let countryCode = countryList[currecyCode];
+    //console.log(countryCode);
+    let newSrc = `https://flagsapi.com/IN/flat/64.png`
+ }
